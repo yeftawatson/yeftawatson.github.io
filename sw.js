66e64afc646ce22a213622b7b642ca4255ee80fb
yeftawatson <yeftanathaniel16@gmail.com>
2023-03-31 07:44:03 +0700

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,18 +1,25 @@
-self.addEventListener('fetch', function (event) {
+self.addEventListener('fetch', function(event) {
   event.respondWith(
-      caches.open(cacheName)
-          .then(function(cache) {
-              cache.match(event.request)
-                  .then( function(cacheResponse) {
-                      if(cacheResponse)
-                          return cacheResponse
-                      else
-                          return fetch(event.request)
-                              .then(function(networkResponse) {
-                                  cache.put(event.request, networkResponse.clone())
-                                  return networkResponse
-                              })
-                  })
-          })
-  )
+    caches.match(event.request).then(function(response) {
+      if (response) {
+        // If the response is found in the cache, return it
+        return response;
+      }
+      // Otherwise, fetch the resource from the network
+      return fetch(event.request).then(function(response) {
+        // Check if we received a valid response
+        if(!response || response.status !== 200 || response.type !== 'basic') {
+          return response;
+        }
+        // Clone the response
+        var responseToCache = response.clone();
+        // Save the response to the cache
+        caches.open('my-cache').then(function(cache) {
+          cache.put(event.request, responseToCache);
+        });
+        // Return the response
+        return response;
+      });
+    })
+  );
 });
