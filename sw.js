5c3d31deddc22d9dcb98d4621a0d8a7b07d69386
yeftawatson <yeftanathaniel16@gmail.com>
2023-03-31 09:05:05 +0700

    Update sw.js

--- sw.js
+++ sw.js
@@ -15,57 +15,28 @@ var URLS = [                            // Add URL you want to cache in this lis
 ]
 
 // Install the service worker and cache the files
-self.addEventListener("install", (event) => {
-  event.waitUntil(
-    caches
-      .open(CACHE_NAME)
-      .then((cache) => cache.addAll(urlsToCache))
-      .then(() => self.skipWaiting())
-  );
-});
-
-// Activate the service worker and delete old caches
-self.addEventListener("activate", (event) => {
-  event.waitUntil(
-    caches
-      .keys()
-      .then((cacheNames) => {
-        return Promise.all(
-          cacheNames
-            .filter((cacheName) => {
-              return cacheName !== CACHE_NAME;
-            })
-            .map((cacheName) => {
-              return caches.delete(cacheName);
-            })
-        );
-      })
-      .then(() => self.clients.claim())
-  );
-});
-
-// Fetch the files from the cache first, then the network
-self.addEventListener("fetch", (event) => {
+self.addEventListener('fetch', function(event) {
   event.respondWith(
-    caches.match(event.request).then((response) => {
+    caches.match(event.request).then(function(response) {
       if (response) {
-        // If the file is found in the cache, return it
+        // If the response is found in the cache, return it
         return response;
-      } else {
-        // If the file is not found in the cache, fetch it from the network
-        return fetch(event.request)
-          .then((response) => {
-            // Cache the fetched file
-            return caches.open(CACHE_NAME).then((cache) => {
-              cache.put(event.request, response.clone());
-              return response;
-            });
-          })
-          .catch((error) => {
-            console.error("Error fetching data:", error);
-            throw error;
-          });
       }
+      // Otherwise, fetch the resource from the network
+      return fetch(event.request).then(function(response) {
+        // Check if we received a valid response
+        if(!response || response.status !== 200 || response.type !== 'basic') {
+          return response;
+        }
+        // Clone the response
+        var responseToCache = response.clone();
+        // Save the response to the cache
+        caches.open('my-cache').then(function(cache) {
+          cache.put(event.request, responseToCache);
+        });
+        // Return the response
+        return response;
+      });
     })
   );
 });
